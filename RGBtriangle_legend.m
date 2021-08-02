0828a53ce48985abc65959e6c5b5a1be2bfb0e22
wywumichelle <35506901+wywumichelle@users.noreply.github.com>
2021-08-02 13:47:36 -0700

    Update and rename RGBtriangle.m to RGBtriangle_legend.m

--- RGBtriangle_legend.m
+++ RGBtriangle_legend.m
@@ -1,4 +1,10 @@
+% MATLAB code for RGB lengend in Figure 3
+% Wu et al., 2020; Nature Communications, 11, 3710, DOI:10.1038/s41467-020-17581-y
+
 clear all; close all;
+
+% filled polygonal regions using PATCH -https://www.mathworks.com/help/matlab/ref/patch.html
+% https://stackoverflow.com/questions/9869167/generate-color-triangle
 
 xy=[0 0; 1 0; 0.5 sqrt(3)/2];
 col=[1 0 0; 0 1 0; 0 0 1];
@@ -8,14 +14,18 @@ axis off
 text(xy(1,1)+0.03, xy(1,2)-0.05,'Snowmelt','HorizontalAlignment', 'center','FontWeight','bold','Fontsize',27,'color','r')
 text(xy(2,1), xy(2,2)-0.05,'ET','HorizontalAlignment', 'center','FontWeight','bold','Fontsize',30,'color','g')
 text(xy(3,1), xy(3,2)+0.05,'Rainfall','HorizontalAlignment', 'center','FontWeight','bold','Fontsize',30,'color','b')
+  
+% case specific input
 rain=[0.605 0.462 0.598 0.420 0.643 0.541 0.613];
 melt=[0 0.295 0.207 0.400 0 0.062 0.316];
+%
 hold on
 
 %plot ([0 1 0.5 0],[0 0 sqrt(3)/2 0],'linewidth',1,'color' ,'k','handlevisibility','off');
 
 yy= rain*(sqrt(3)/2)
 xx=(1-melt)-yy/sqrt(3)
+  
 scatter(xx,yy,50,'k','filled')
 %plot([0.5 0.5],[0 0.5/sqrt(3)],'w')
 %plot([0.25 0.5],[sqrt(3)/4 0.5/sqrt(3)],'w')
